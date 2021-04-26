f039617987d2f0c9909a63352927f11a19ae48a0
Maryam Cheema <maryamsaadat@Maryams-MacBook-Pro.local>
2021-04-26 12:21:38 +0300

    Article and product css

--- my-app/src/components/Articles.jsx
+++ my-app/src/components/Articles.jsx
@@ -5,18 +5,24 @@ import './articles.css'
 
 function Articles(props)
 {
+    function sendProductID(productID)
+    {
+        //console.log("hello",productID)
+        localStorage.setItem('productID', productID)
+        //console.log(localStorage.getItem('productID'))
+    }
     return(
         <Card className="entire-card" style={{ width: '15rem' }} >
-            <Card.Img variant="top" src={props.image} />
+            <Card.Img variant="top" src={process.env.PUBLIC_URL + props.image} />
             <Card.Body class="card-body">
                 {/* add name of the product */}
                 <Card.Title>{props.title}</Card.Title>
                 {/* add whether the product is two peice or three piece */}
                 <Card.Text>{props.piece}</Card.Text>
                 {/* add price of the product */}
-                <Card.Text>{props.price}</Card.Text>
+                <Card.Text>Price: Rs {props.price}</Card.Text>
                 {/* Butoon takes the customer to the product page  */}
-                <Button variant="outline-dark" href="/product">View Product</Button>
+                <Button variant="outline-dark" href="/product" onClick = {()=>{sendProductID(props.productid)}}>View Product</Button>
             </Card.Body>
         </Card>
     )
